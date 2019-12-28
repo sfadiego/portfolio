c7ce0f27dba71324dd7aa3c3bc30d893cf093a37
diego silva facio <sfadiego@gmail.com>
2019-12-28 14:10:07 -0600

    contacto y template

--- routes/index.js
+++ routes/index.js
@@ -5,6 +5,10 @@ module.exports = (app)=>{
     res.render('index');
   });
 
+  app.get('/contact', function(req, res, next) {
+    res.render('contact');
+  });
+
   app.get('/downloadcv', function(req, res, next) {
     let file = path.resolve(__dirname,'../public/filescv/cvdiego.pdf');
     res.sendFile(file);
